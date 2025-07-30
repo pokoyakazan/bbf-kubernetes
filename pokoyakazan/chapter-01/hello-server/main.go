fbecd52bbd1b80d01d1c6a1ea7f1e8b5893233df
pokoyakazan <okuyama1625@gmail.com>
2025-07-31 08:48:36 +0900

    2025-07-31 progress

--- pokoyakazan/chapter-01/hello-server/main.go
+++ pokoyakazan/chapter-01/hello-server/main.go
@@ -8,10 +8,13 @@ import (
 )
 
 func main() {
+	// "/"で始まるパスへのリクエストを受け付けるハンドラ
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
 		fmt.Fprintf(w, "Hello, World!")
 	})
+	// ログにサーバー起動のメッセージを出力
 	log.Println("Starting server on port 8080")
+	// ポート8080でHTTPサーバーを起動
 	err := http.ListenAndServe(":8080", nil)
 	if err != nil {
 		log.Fatal(err)
